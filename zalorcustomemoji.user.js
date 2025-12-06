9a77037bd555cef187194542fa83b3c4e7e1b5eb
Thành Lã <thanhladev@gmail.com>
2025-12-06 16:09:10 +0700

    Bump version to 1.8.1

--- zalorcustomemoji.user.js
+++ zalorcustomemoji.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Zalo Custom Reactions
-// @version      1.8
+// @version      1.8.1
 // @description  Zalo web custom reaction
 // @author       Anhwaivo
 // @match        https://*.zalo.me/*
